fa72b78f580e7d459f9a440e4cbf08d01cca4dfe
volnistii11 <cor.fascio@gmail.com>
2024-05-17 22:05:49 +0400

    change kafka payload

--- transfer/internal/app/api/kafka/consumer_group/handler.go
+++ transfer/internal/app/api/kafka/consumer_group/handler.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/IBM/sarama"
+	"github.com/volnistii11/accounting_service/transfer/internal/app/models"
 )
 
 type ConsumerGroupHandler struct {
@@ -33,7 +34,12 @@ func (h *ConsumerGroupHandler) ConsumeClaim(session sarama.ConsumerGroupSession,
 				return nil
 			}
 
-			msg := convertMsg(message)
+			msg, err := convertMsg(message)
+			if err != nil {
+				log.Printf("Err: %s", err.Error())
+			}
+			log.Printf("Message payload: %+v", msg.Payload)
+
 			data, err := json.Marshal(msg)
 			if err != nil {
 				log.Printf("Err: %s", err.Error())
@@ -49,19 +55,26 @@ func (h *ConsumerGroupHandler) ConsumeClaim(session sarama.ConsumerGroupSession,
 }
 
 type Msg struct {
-	Topic     string `json:"topic"`
-	Partition int32  `json:"partition"`
-	Offset    int64  `json:"offset"`
-	Key       string `json:"key"`
-	Payload   string `json:"payload"`
+	Topic     string               `json:"topic"`
+	Partition int32                `json:"partition"`
+	Offset    int64                `json:"offset"`
+	Key       string               `json:"key"`
+	Payload   models.MoneyTransfer `json:"payload"`
 }
 
-func convertMsg(in *sarama.ConsumerMessage) Msg {
-	return Msg{
+func convertMsg(in *sarama.ConsumerMessage) (*Msg, error) {
+	var moneyTransfer models.MoneyTransfer
+
+	err := json.Unmarshal(in.Value, &moneyTransfer)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Msg{
 		Topic:     in.Topic,
 		Partition: in.Partition,
 		Offset:    in.Offset,
 		Key:       string(in.Key),
-		Payload:   string(in.Value),
-	}
+		Payload:   moneyTransfer,
+	}, err
 }
